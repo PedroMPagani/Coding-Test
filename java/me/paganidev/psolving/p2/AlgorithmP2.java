af06355f3df3329925a43c517b3dda71362479ec
Pedro Paulo Monte Pagani <56891617+PedroMPagani@users.noreply.github.com>
2021-12-21 12:10:26 -0300

    Update AlgorithmP2.java

--- java/me/paganidev/psolving/p2/AlgorithmP2.java
+++ java/me/paganidev/psolving/p2/AlgorithmP2.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class AlgorithmP2 {
 
+    // stream test
     public List<List<Character>> array(List<String> list){
         return list.stream().filter(s->s != null && !s.contains("&") && !s.contains("!")).map(q->q.chars().filter(q1->q1 % 10 != 0))
                 .map(s->s.mapToObj(f->(char)f).sorted(Comparator.comparingInt(x -> (char)x).reversed()).toList()).toList();
